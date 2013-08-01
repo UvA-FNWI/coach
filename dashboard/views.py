7ad4d38dd166f225d415b7ba3e72c28765784114
Chiel Kooijman <chiel999@gmail.com>
2013-08-01 14:25:11 +0200

    Removed default functions from API, because they do not use rights.

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.conf import settings
-#from recommendation import apriori
+from recommendation.recommend import recommend
 import tincan_api
 import json
 
@@ -14,40 +14,20 @@ ENDPOINT = settings.TINCAN['endpoint']
 
 tincan = tincan_api.TinCan(USERNAME, PASSWORD, ENDPOINT)
 
-from pprint import pprint
+
 # dashboard
 def index(request):
-    pprint(request)
-    print request.session
     return render(request, 'dashboard/index.html',
                   {'session': request.session})
 
 
 # Recommendations
-def apriori(request, minsup=0):
-    # apriori.apriori(float(minsup))
-    return HttpResponse("apriori, minsup=%s" % float(minsup))
+def apriori(request):
+    recommend.recommend
 
 
 # TinCan
-def tincan_all(request):
+def tincan(request):
     obj = {'actor': {'name': request.user, 'objectType': 'Agent'}}
     tc_resp = tincan.getFilteredStatements(obj)
     return HttpResponse(json.dumps(tc_resp['statements']))
-
-def tincan_filtered(request):
-    tc_resp = tincan.getFilteredStatements(request.GET)
-    print tc_resp
-    return HttpResponse(tc_resp)
-
-def tincan_id(request, statement_id):
-    tc_resp = tincan.getStatementByID(statement_id)
-    return HttpResponse(tc_resp)
-
-def tincan_submit(request):
-    post = request.POST
-    if isinstance(post, list):
-        tc_resp = tincan.submitStatementList(post)
-    else:
-        tc_resp = tincan.submitStatement(post)
-    return HttpResponse(tc_resp)
