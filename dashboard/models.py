145a6fd252936caf3d683afe2276b92300e7c22b
Chiel Kooijman <chiel999@gmail.com>
2013-08-01 13:16:06 +0200

    Added a model for rocommendations.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Recommendation(models.Model):
+    task_url = models.URLField()
+    items_hash = models.IntegerField()
+    recommendation_url = models.URLField()
+    recommendation_name = models.CharField(max_length=255)
+    confidence = models.FloatField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.recommendation_name
+
+    def __html__(self):
+        return '<a href="' + str(recommendation_url) + '" >' +\
+               str(recommendation_name) + '</a>'
