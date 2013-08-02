6ab88cc96db4a7da1bf4e84b4cad4b5f9840ee64
mslatour <sanderlator@gmail.com>
2013-08-02 16:50:34 +0200

    Changed hash_item to BigInteger for MySQL compat

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Recommendation(models.Model):
-    item_hash = models.IntegerField()
+    item_hash = models.BigIntegerField()
     milestone = models.URLField()
     url = models.URLField()
     name = models.CharField(max_length=255)
