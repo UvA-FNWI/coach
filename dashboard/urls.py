2d20d97f112d7ca76f39635a8749384737015817
Chiel Kooijman <chiel999@gmail.com>
2013-08-02 16:24:21 +0200

    Added Recommendations to admin page.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
+from django.contrib import admin
+admin.autodiscover()
 
 from dashboard import views
 
@@ -6,6 +8,8 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^recommendations$', views.generate_recommendations,
             name='generate recommendations'),
+        url(r'^recommend$', views.get_recommendations,
+            name='get recommendations'),
         #url(r'^apriori/(?P<minsup>\d+(.\d+)?)/$', views.apriori, name='apriori'),
         url(r'^api/statements$', views.get_statements, name='get statements'),
         )
