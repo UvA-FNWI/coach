929260587904b89508aecee777095db0108b2b6e
Chiel Kooijman <chiel999@gmail.com>
2013-08-02 16:28:20 +0200

    Use new recommendation dictionary format in view.

--- dashboard/views.py
+++ dashboard/views.py
@@ -27,9 +27,16 @@ def index(request):
 
 
 # Recommendations
+
+def get_recommendations(request):
+    item_hash = hash(sort(request.POST))
+    Recommendation.objects.get(item_hash=item_hash)
+    # FIXME do something
+
+
 @transaction.commit_manually
 def generate_recommendations(request):
-    r_list, r_dict = recommend(minsup=3, minconf=.5)
+    r_list, r_dict = recommend(minsup=4, minconf=.5)
 
     # Add recommendations to database
     Recommendation.objects.all().delete()
@@ -40,8 +47,7 @@ def generate_recommendations(request):
         support = r['support']
         milestone = r['milestone']
         # Store each recommendation separately
-        for consequent in r['consequent']:
-            url = consequent[1]
+        for url in r['consequent']:
             name = r_dict[url][0]
             description = r_dict[url][1]
             i += 1
