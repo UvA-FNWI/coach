2bc433e45e8bea3958063a32d7a7e687b18c8d03
mslatour <sanderlatour@gmail.com>
2015-09-05 19:29:06 +0200

    Added admin list display fields for assessments

--- course/admin.py
+++ course/admin.py
@@ -4,5 +4,8 @@ from .models import *
 class CourseAdmin(admin.ModelAdmin):
     exclude = ('last_updated',)
 
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ("title", "start_date", "end_date", "url")
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Assessment)
+admin.site.register(Assessment, AssessmentAdmin)
